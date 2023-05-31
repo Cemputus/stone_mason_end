353398c44854eee1e21dc2f139eb0dd132e7e25d
Emmanuel Nsubuga <107301618+Cemputus@users.noreply.github.com>
2023-05-31 09:53:04 +0300

    Another Version

---
+++ stone_mason_end_ 2.py
@@ -0,0 +1,45 @@
+from stanfordkarel import *
+def main():
+    gpt()
+    cen()
+    turn_left()
+    loop1()
+    gpt()
+    cen()   
+    turn_right()
+    loop1()
+    pick_beeper()
+    turn_right()
+    cen()
+    turn_left()
+    loop1()
+    gpt()
+    cen()
+
+
+
+    
+    
+def turn_right():
+    for x in range(3):
+        turn_left()
+    
+def cen():
+    for i in range(4):
+        chat()
+
+def loop1():
+    for i in range(4):
+        move()    
+
+
+def chat():
+    move()
+    pick_beeper()
+    
+def gpt():
+    pick_beeper()
+    turn_left()    
+    
+if __name__ == "__main__":
+    run_karel_program()
